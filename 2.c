8bed169eaf4760ef4ef5ee71912478dbf84d6bd3
BittuKumar77 <110903496+BittuKumar77@users.noreply.github.com>
2022-09-26 17:24:47 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,33 @@
+//2. Write a program to calculate the product of two matrices each of order 3x3.
+
+#include <stdio.h>
+int main()
+{
+    int row, col, sumOfMatrix = 0;
+    printf("Enter Row and Column of Matrix: ");
+    scanf("%d%d", &row, &col);
+
+    int matrix[row][col];
+    printf("Enter Element in %d*%d Matrix: \n", row, col);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+            sumOfMatrix += matrix[i][j];
+        }
+    }
+
+    // print Matrix
+    printf("Given Matrix: \n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+
+    // matrix sum
+    printf("Matrix All Element Sum is: %d\n", sumOfMatrix);
+    return 0;
+}
